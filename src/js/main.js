473a23b5d8989ddbcfc4997f73bb7335de791dbe
Lei Xiao <97568440+lxiao36@users.noreply.github.com>
2022-04-27 18:05:25 -0400

    Project 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,79 @@
+var dragged;
 
+/* events fired on the draggable target */
+document.addEventListener("drag", function (event) {}, false);
+
+/* Makes draggable figure transparent */
+document.addEventListener(
+  "dragstart",
+  function (event) {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.style.opacity = 0.5;
+  },
+  false
+);
+
+document.addEventListener(
+  "dragend",
+  function (event) {
+    // reset the transparency
+    event.target.style.opacity = "";
+  },
+  false
+);
+
+/* events fired on the drop targets */
+document.addEventListener(
+  "dragover",
+  function (event) {
+    // prevent default to allow drop
+    event.preventDefault();
+  },
+  false
+);
+
+/* Makes drop elements transparent */
+document.addEventListener(
+  "dragenter",
+  function (event) {
+    // highlight potential drop target when the draggable element enters it
+    if (event.target.className == "dropzone") {
+      event.target.style.opacity = 0.5;
+    }
+  },
+  false
+);
+
+document.addEventListener(
+  "dragleave",
+  function (event) {
+    // reset background of potential drop target when the draggable element leaves it
+    if (event.target.className == "dropzone") {
+      event.target.style.opacity = 1;
+    }
+  },
+  false
+);
+
+document.addEventListener(
+  "drop",
+  function (event) {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    // move dragged elem to the selected drop target
+    if (event.target.className == "dropzone") {
+      event.target.style.background = "";
+      dragged.parentNode.removeChild(dragged);
+      event.target.appendChild(dragged);
+      console.log(window.location.hash.replace("card", ""));
+      window.location.hash = `card${
+        parseInt(window.location.hash.replace("#card", "")) + 1
+      }`;
+    }
+  },
+  false
+);
+
+/* code credit: "https://developer.mozilla.org/en-US/docs/Web/API/Document/drag_event" */
